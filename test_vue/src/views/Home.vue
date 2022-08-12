d4fce62963a47a19e1f4317e117327de6e6eddb6
huangj1234 <2431418188@qq.com>
2022-08-12 13:13:34 +0800

    router
    路由我也弄了一下，不知道有没有问题，你们可以看看

---
+++ test_vue/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>芜湖搞好啦</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
