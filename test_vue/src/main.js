b1ade0004a82906027c037133be2657f32747cdf
huangj1234 <2431418188@qq.com>
2022-08-12 13:34:45 +0800

    el的插件引用

--- test_vue/src/main.js
+++ test_vue/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
+import ElementUI from 'element-ui'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI, { size: 'small' })
 
 new Vue({
   router,
